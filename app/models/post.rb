a895aeeec774838b5757650e1525979be98f6eba
tuckerTU <tuckerishere@gmail.com>
2021-07-09 13:01:07 -0600

    commit tree e83928766d647dec76eee8767d803427e017365d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+  
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_include
+
+    CLICKBAIT_PATTERNS = [
+        "Won't Believe",
+        "Secret",
+        "Top[number]",
+        "Guess"
+    ]
+
+    def title_include
+        if title.present? && CLICKBAIT_PATTERNS.none? {|p| title.match(p)}
+            errors.add(:title, "title not clickbait-y enough")
+        end
+    end
 end
